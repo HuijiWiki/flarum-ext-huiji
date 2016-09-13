9d2e768996b12f79b7dfb7fed56bb4a463b76cec
c86jeff <zhangjiexin0101@gmail.com>
2016-09-13 16:30:20 +0800

    first commit

---
+++ js/forum/dist/extension.js
@@ -0,0 +1,41 @@
+'use strict';
+
+System.register('reasno/flarum-huiji/main', ['flarum/extend', 'flarum/components/HeaderSecondary', 'flarum/components/UserPage'], function (_export, _context) {
+  "use strict";
+
+  var extend, HeaderSecondary, UserPage;
+  return {
+    setters: [function (_flarumExtend) {
+      extend = _flarumExtend.extend;
+    }, function (_flarumComponentsHeaderSecondary) {
+      HeaderSecondary = _flarumComponentsHeaderSecondary.default;
+    }, function (_flarumComponentsUserPage) {
+      UserPage = _flarumComponentsUserPage.default;
+    }],
+    execute: function () {
+      app.initializers.add('reasno-flarum-ext-huiji', function () {
+        extend(HeaderSecondary.prototype, 'items', function (items) {
+          if (items.has('signUp')) {
+            items.replace('signUp', m(
+              'a',
+              { href: 'http://www.huiji.wiki/wiki/Special:CreateAccount?returnto=forum:', className: 'Button Button-signUp' },
+              '注册'
+            ));
+          }
+          if (items.has('logIn')) {
+            items.replace('logIn', m(
+              'a',
+              { href: 'http://www.huiji.wiki/wiki/Special:Login?returnto=forum:', className: 'Button Button-logIn' },
+              '登录'
+            ));
+          }
+        });
+        extend(UserPage.prototype, 'navItems', function (items) {
+          if (items.has('settings')) {
+            items.remove('settings');
+          }
+        });
+      });
+    }
+  };
+});
---
+++ js/forum/src/main.js
@@ -0,0 +1,18 @@
+import { extend } from 'flarum/extend';
+import HeaderSecondary from 'flarum/components/HeaderSecondary';
+import UserPage from 'flarum/components/UserPage';
+app.initializers.add('reasno-flarum-ext-huiji', function() {
+  extend(HeaderSecondary.prototype, 'items', function(items) {
+    if (items.has('signUp')){
+    	items.replace('signUp', <a href="http://www.huiji.wiki/wiki/Special:CreateAccount?returnto=forum:" className="Button Button-signUp">注册</a>);
+    }
+    if (items.has('logIn')){
+	items.replace('logIn', <a href="http://www.huiji.wiki/wiki/Special:Login?returnto=forum:" className="Button Button-logIn">登录</a>);
+    }
+  });
+  extend(UserPage.prototype, 'navItems', function(items) {
+    if (items.has('settings')){
+        items.remove('settings');
+    }
+  });
+});
