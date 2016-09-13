914bb26e1f9faa8a3290fc0b38de571e7c1efcf8
c86jeff <zhangjiexin0101@gmail.com>
2016-09-13 19:05:01 +0800

    stuff

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -1,12 +1,14 @@
 'use strict';
 
-System.register('reasno/flarum-huiji/main', ['flarum/extend', 'flarum/components/HeaderSecondary', 'flarum/components/SessionDropdown'], function (_export, _context) {
+System.register('reasno/flarum-huiji/main', ['flarum/extend', 'flarum/components/HeaderPrimary', 'flarum/components/HeaderSecondary', 'flarum/components/SessionDropdown'], function (_export, _context) {
   "use strict";
 
-  var extend, HeaderSecondary, SessionDropdown;
+  var extend, HeaderPrimary, HeaderSecondary, SessionDropdown;
   return {
     setters: [function (_flarumExtend) {
       extend = _flarumExtend.extend;
+    }, function (_flarumComponentsHeaderPrimary) {
+      HeaderPrimary = _flarumComponentsHeaderPrimary.default;
     }, function (_flarumComponentsHeaderSecondary) {
       HeaderSecondary = _flarumComponentsHeaderSecondary.default;
     }, function (_flarumComponentsSessionDropdown) {
@@ -18,14 +20,14 @@ System.register('reasno/flarum-huiji/main', ['flarum/extend', 'flarum/components
           if (items.has('signUp')) {
             items.replace('signUp', m(
               'a',
-              { href: 'http://www.huiji.wiki/wiki/Special:CreateAccount?returnto=forum:', className: 'Button Button-signUp' },
+              { href: 'http://www.huiji.wiki/wiki/Special:CreateAccount?returnto=Special:RedirectToForum', className: 'Button Button-signUp' },
               '注册'
             ));
           }
           if (items.has('logIn')) {
             items.replace('logIn', m(
               'a',
-              { href: 'http://www.huiji.wiki/wiki/Special:Login?returnto=forum:', className: 'Button Button-logIn' },
+              { href: 'http://www.huiji.wiki/wiki/Special:Login?returnto=Special:RedirectToForum', className: 'Button Button-logIn' },
               '登录'
             ));
           }
@@ -34,6 +36,13 @@ System.register('reasno/flarum-huiji/main', ['flarum/extend', 'flarum/components
           if (items.has('logOut')) {
             items.remove('logOut');
           }
+        });
+        extend(HeaderPrimary.prototype, 'items', function (items) {
+          items.add('huiji', m(
+            'a',
+            { href: 'http://www.huiji.wiki', id: 'huiji-logo', className: 'Button Button--link' },
+            m('img', { alt: 'Logo', src: 'http://cdn.huijiwiki.com/www/resources/assets/huiji_white.png' })
+          ));
         });
       });
     }
--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -1,18 +1,22 @@
 import { extend } from 'flarum/extend';
+import HeaderPrimary from 'flarum/components/HeaderPrimary';
 import HeaderSecondary from 'flarum/components/HeaderSecondary';
 import SessionDropdown from 'flarum/components/SessionDropdown';
 app.initializers.add('reasno-flarum-ext-huiji', function() {
   extend(HeaderSecondary.prototype, 'items', function(items) {
     if (items.has('signUp')){
-    	items.replace('signUp', <a href="http://www.huiji.wiki/wiki/Special:CreateAccount?returnto=forum:" className="Button Button-signUp">注册</a>);
+    	items.replace('signUp', <a href="http://www.huiji.wiki/wiki/Special:CreateAccount?returnto=Special:RedirectToForum" className="Button Button-signUp">注册</a>);
     }
     if (items.has('logIn')){
-	items.replace('logIn', <a href="http://www.huiji.wiki/wiki/Special:Login?returnto=forum:" className="Button Button-logIn">登录</a>);
+	items.replace('logIn', <a href="http://www.huiji.wiki/wiki/Special:Login?returnto=Special:RedirectToForum" className="Button Button-logIn">登录</a>);
     }
   });
   extend(SessionDropdown.prototype, 'items', function(items) {
     if (items.has('logOut')){
         items.remove('logOut');
     }
+  });
+  extend(HeaderPrimary.prototype, 'items', function(items){
+    items.add('huiji', <a href="http://www.huiji.wiki" id="huiji-logo" className="Button Button--link"><img alt="Logo" src="http://cdn.huijiwiki.com/www/resources/assets/huiji_white.png"/></a>);
   });
 });
